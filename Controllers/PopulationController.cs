1e3a2192397d939a8c22ece29fcfb9159bfee487
MrCandle <rodrigofvela@gmail.com>
2019-03-16 18:54:31 -0300

    Population controller and query search

---
+++ Controllers/PopulationController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using multithreading.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace multithreading.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PopulationController : ControllerBase
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+  
+        public PopulationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        
+        // GET api/population/
+        [HttpGet]
+        public async Task<ActionResult<List<Country>>> GetAll()
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("https://restcountries.eu");
+            string content = await client.GetStringAsync("/rest/v2/all");
+            return Ok(JsonConvert.DeserializeObject<List<Country>>(content));
+        }
+
+        // GET api/population/name/uk
+        [HttpGet("name/{str}")]
+        public async Task<ActionResult<List<Country>>> GetByQueryString(string str)
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("https://restcountries.eu");
+            string content = await client.GetStringAsync(String.Format("/rest/v2/name/{0}", str));
+            Console.WriteLine(content);
+            return Ok(JsonConvert.DeserializeObject<List<Country>>(content));
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,7 @@ namespace multithreading
                 c.SwaggerDoc("v1", new Info { Title = "Multithreading API", Version = "v1" });
             });
 
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
